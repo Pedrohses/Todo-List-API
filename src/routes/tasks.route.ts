35603d1d8800e7a9be6b3dfddc46a87b3af8f45a
Pedrohses <desouzaesilvap@gmail.com>
2025-08-08 20:31:42 -0300

    feat: create CRUD routes

--- src/routes/tasks.route.ts
+++ src/routes/tasks.route.ts
@@ -1,7 +1,88 @@
 import { FastifyInstance } from "fastify";
+import { knex } from "../knex";
+import { isLogged } from "../middlewares/is-logged";
+import z from "zod";
+import { randomUUID } from "crypto";
+import { isYourTask } from "../middlewares/is-your-task";
 
 export const tasks = (app: FastifyInstance) => {
- app.get('/', async (request, reply) => {
-  return reply.status(200).send('Hello world!');
- }) 
+
+  app.post(
+    '/',
+    { preHandler: isLogged },
+    async (request, reply) => {
+      const taskSchema = z.object({
+        name: z.string(),
+        description: z.string(),
+        is_complete: z.boolean()
+      })
+
+      const { name, description, is_complete } = taskSchema.parse(request.body)
+      const { session_id } = request.cookies
+
+      const { id } = await knex('users')
+      .select('id')
+      .where('session_id', session_id)
+      .first()
+
+      await knex('tasks').insert({
+        id: randomUUID(),
+        user_id: id,
+        name: name,
+        description: description,
+        is_complete: is_complete,
+        created_at: new Date()
+      })
+
+      return reply.status(201).send('Task created with success')
+  })
+
+  app.get(
+    '/',
+    { preHandler: [isLogged] },
+    async ( request, reply ) => {
+    const { session_id } = request.cookies
+
+    const { id } = await knex('users')
+      .select('id')
+      .where('session_id', session_id)
+      .first()
+
+    const tasks = await knex('tasks').select('*').where('user_id', id)
+
+    return reply.status(200).send(tasks)
+  })
+
+  app.put(
+    '/:id',
+    { preHandler: [ isLogged, isYourTask ]},
+    async ( request, reply ) => {
+      const taskSchema = z.object({
+        name: z.string().optional(),
+        description: z.string().optional(),
+        is_complete: z.boolean().optional()
+      })
+      const { name, description, is_complete } = taskSchema.parse(request.body)
+      const { id } = request.params as { id: string }
+
+      await knex('tasks').update({
+        name: name,
+        description: description,
+        is_complete: is_complete
+      }).where('id', id)
+
+      return reply.status(204).send()
+    }
+  )
+
+  app.delete(
+    '/:id',
+    { preHandler: [isLogged, isYourTask] },
+    async ( request, reply ) => {
+    const { id } = request.params as { id: string }
+
+    await knex('tasks').delete().where('id', id)
+
+    return reply.status(204).send()
+  })
 }
--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -1,7 +1,45 @@
 import { FastifyInstance } from "fastify";
+import { knex } from "../knex";
+import z, { uuid } from "zod";
+import { request } from "http";
+import { randomUUID } from "crypto";
+import { isLogged } from "../middlewares/is-logged";
 
 export const users = (app: FastifyInstance) => {
- app.get('/', async (request, reply) => {
-  return reply.status(200).send('Hello world!');
- }) 
+
+  app.post('/', async (request, reply) => {
+    const userSchema = z.object({
+      name: z.string(),
+      email: z.string(),
+    })
+
+    const { name, email } = userSchema.parse(request.body)
+    const session_id = randomUUID();
+
+    await knex('users').insert({
+      id: randomUUID(),
+      session_id: session_id,
+      name: name,
+      email: email,
+      created_at: new Date()
+    })
+
+    reply
+      .setCookie('session_id', session_id)
+      .status(201)
+      .send('User created with success!')
+  })
+  
+  app.get(
+   '/', 
+   { preHandler: isLogged },
+   async (request, reply) => {
+   const { session_id } = request.cookies
+  
+   const user = await knex('users')
+     .select('*')
+     .where('session_id', session_id)
+  
+   return reply.status(200).send(user)
+  })
 }
